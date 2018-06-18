9ba788a947414e28302e489adb14613f7f367802
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-06-17 22:40:10 -0400

    sync

--- init.zsh
+++ init.zsh
@@ -1,24 +1,20 @@
-p6df::modules::emacs::version() { echo "0.0.1" }
-p6df::modules::emacs::deps()    { 
-	ModuleDeps=()
+p6df::modules::vim::version() { echo "0.0.1" }
+p6df::modules::vim::deps()    { 
+	ModuleDeps=(p6m7g8/p6vim)
 }
 
-p6df::modules::emacs::external::git() {
-  
-  git clone https://github.com/magnars/.emacs.d
-  git clone https://github.com/hlissner/doom-emacs
-  git clone https://github.com/pgollucci/emacs
+p6df::modules::vim::external::git() {
 }
 
-p6df::modules::emacs::external::brew() {
+p6df::modules::vim::external::brew() {
 
-  brew install emacs --with-cocoa --with-librsvg --with-mailutils --with-modules
-  brew install mu --with-emacs
-  brew install offlineimap
-  brew install w3m
+  brew install vim --with-cocoa --with-librsvg --with-mailutils --with-modules
 }
 
-p6df::modules::emacs::init() {
+p6df::modules::vim::home::symlink() {
 
-    alias ek="ps -efwww | awk '/[eE]macs/ { print \$2 }' | xargs kill -9"
+  # XXX: .vim.d + selection
+}
+
+p6df::modules::vim::init() {
 }
